810b1bda2e76fa4fff268412154e7e4852e15a2c
Erik Wärmegård <erikwarmegard@outlook.com>
2020-02-25 18:23:28 +0100

    AnaScript Update
    Dolores makes statistical calculations now. Pearsons R, Covariance and plots correlation graphs. Dolores page is now avaiable. Conlusions are made regarding the correlation between my workout habits and my sleeping habits, considering a very limited dataset.

--- dolores.py
+++ dolores.py
@@ -8,44 +8,36 @@ from numpy import cov
 from numpy import genfromtxt
 from numpy.random import randn
 from numpy.random import seed
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
 import pandas as pd
 import seaborn as sns
 
 
-# seed random number generator
-seed(1)
-# prepare data
-data1 = 20 * randn(1000) + 100
-data2 = data1 + (10 * randn(1000) + 50)
+df = pd.read_csv("sleep-calories.csv", sep=',', names = ['Date', 'Sleep', 'Calories'])
 
-# sleep_data = pd.read_csv('sleep.csv', sep=',', names=['StartTime', 'EndTime', 'MinutesAsleep', 'MinutesAwake', 'NumberofAwakenings', 'TimeinBed', 'MinutesREMSleep', 'MinutesLightSleep', 'MinutesDeepSleep'])
-# activity_data = pd.read_csv('activity.csv', sep=',', names=['Date', 'CaloriesBurned', 'Steps', 'Distance', 'Floors', 'Minutes Sedentary', 'Minutes Lightly Active', 'Minutes Fairly Active', 'Minutes Very Active', 'Activity Calories'])
-
-minutes_asleep = pd.read_csv("sleep.csv", sep=',', usecols = ['MinutesAsleep'])
-calories_burned = pd.read_csv("activity.csv", sep=",", usecols = ['CaloriesBurned'])
-
-print(minutes_asleep)
-
-
-
+dates = df.Date
+sleep_data = df.Sleep
+kcal = df.Calories
 
 # Positive covariance --> The variables change in the same direction. Negative --> The variables change in the opposite direction. covariance = 0 --> Independent variables.
-covariance = cov(data1, data2)
+covariance = cov(sleep_data, kcal)
+print('covariance', covariance)
+
+
+scatter = sns.lmplot(x='Calories', y='Sleep', data = df, fit_reg=True)
+#plt.grid()
+#plt.show()
+
+
+
 # The Pearson correlation can be used to summarize the strength of the linear relationshop between two data samples.
 # -1 < corr < 1. Values between -0.5 and 0.5 indicates a less notable correlation, where as values smaller than -0.5 and greater than 0.5 indicates a notable correlation
-corr, _ = pearsonr(data1, data2)
+
 
 
 # summarize
-# print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
-# print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
-# print('Pearsons correlation: %.3f' % corr)
-# print(covariance)
-
-# plot
-pyplot.scatter(minutes_asleep, calories_burned)
-
-pyplot.grid()
-pyplot.show()
+print('data1: mean=%.3f stdv=%.3f' % (mean(sleep_data), std(sleep_data)))
+print('data2: mean=%.3f stdv=%.3f' % (mean(kcal), std(kcal)))
+corr, _ = pearsonr(sleep_data, kcal)
+print('Pearsons correlation: %.3f' % corr)
